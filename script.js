8c1aa4d8fc711e3aa6807aff4cf0abbb601145bd
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-05-31 13:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const sounds = [
+    "applause",
+    "boo",
+    "gasp",
+    "tada",
+    "victory",
+    "wrong"
+];
+
+const buttonsContainer = document.getElementById("buttons");
+
+let currentAudio = null;
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        currentAudio = new Audio(`sounds/${sound}.mp3`);
+        currentAudio.play();
+    });
+
+    buttonsContainer.appendChild(btn);
+});
+
+const stopBtn = document.createElement("button");
+stopBtn.classList.add("stop");
+stopBtn.innerText = "Stop";
+
+stopBtn.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
+
+buttonsContainer.appendChild(stopBtn);
